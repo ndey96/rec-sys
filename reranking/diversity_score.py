1e74c7c69b43f91b7926d1bc340119a0e75cbba7
Nolan Dey <ndey96@gmail.com>
2019-02-11 17:30:02 -0500

    Add broken example to diversity code

--- reranking/diversity_score.py
+++ reranking/diversity_score.py
@@ -1,33 +1,55 @@
+import pandas as pd
 songs_df = pd.read_hdf('data/full_msd_with_audio_features.h5', key='df')
 
+
+def prepare_song_id(song_id):
+  return ('b\'' + song_id + '\'')
+
+
 def get_diversity(song_vector_list):
-    dissim = 0
-    n = len(song_vector_list)
-    
-    for i in range(n):
-        for j in range(n):
-            dissim += np.linalg.norm(song_vector_list[i] - song_vector_list[j])
-            
-    return dissim/((n/2)*(n-1))
+  dissim = 0
+  n = len(song_vector_list)
+  print(n)
+  print(n / 2)
+  print(((n / 2) * (n - 1)))
+  for i in range(n):
+    for j in range(n):
+      dissim += np.linalg.norm(song_vector_list[i] - song_vector_list[j])
 
-def convert_recs_to_embeddings(rec_list):
-    embeddings_list = []
-    
-    for rec_ids in rec_list:
-        #print(prepare_song_id(rec_ids[0]))
-        if (prepare_song_id(rec_ids[0]) in songs_df.song_id.values):
-            row = songs_df.loc[songs_df['song_id'] == prepare_song_id(rec_ids[0])]
-            embedding = row.iloc[:,12:]
-            embedding = np.array(embedding.values.tolist()[0])
-            embeddings_list.append(embedding)
-        
-    return embeddings_list
+  return dissim / ((n / 2) * (n - 1))
 
-def get_diversity_score(rec_list):
-    # convert to list of embeddings
-    embedding_list = convert_recs_to_embeddings(rec_list)
-    
-    # calculate diversity
-    return get_diversity(embedding_list)
-    
-    
+
+def convert_recs_to_embeddings(song_ids):
+  embeddings_list = []
+
+  for song_id in song_ids:
+    #print(prepare_song_id(song_id))
+    if (prepare_song_id(song_id) in songs_df.song_id.values):
+      row = songs_df.loc[songs_df['song_id'] == prepare_song_id(song_id)]
+      embedding = row.iloc[:, 12:]
+      embedding = np.array(embedding.values.tolist()[0])
+      embeddings_list.append(embedding)
+
+  return embeddings_list
+
+
+def get_diversity_score(song_ids):
+  # convert to list of embeddings
+  embedding_list = convert_recs_to_embeddings(song_ids)
+
+  # calculate diversity
+  return get_diversity(embedding_list)
+
+
+song_ids = list(songs_df.head(100).song_id)
+print(len(song_ids))
+print(get_diversity_score(song_ids))
+
+feature_MUSIC_dict = {
+    'danceability': np.array([-0.37, 0.05, -0.35, 0.08, 0.43]),
+    'energy': np.array([-0.64, -0.46, -0.13, 0.66, -0.03]),
+    'instrumentalness': np.array([0.20, -0.47, 0.28, 0.09, -0.01]),
+    'liveness': np.array([-0.69, -0.12, -0.07, 0.43, 0.02]),
+    'loudness': np.array([-0.58, -0.19, -0.44, 0.79, -0.21]),
+    'valence': np.array([-0.04, 0.18, 0.24, -0.34, 0.18]),
+}
