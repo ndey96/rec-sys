f5b405eb3702ddee47b4424df00c7868c1d14c4b
Nolan Dey <ndey96@gmail.com>
2019-02-12 00:41:18 -0500

    Speed up diversity score calculation by 1100x through vectorization

--- reranking/diversity_score.py
+++ reranking/diversity_score.py
@@ -1,58 +1,33 @@
 import pandas as pd
 import numpy as np
+import time
+from scipy.spatial.distance import pdist
 
-songs_df = pd.read_hdf('data/full_msd_with_audio_features.h5', key='df')
+embedding_cols = [
+#     'year',
+    'acousticness',
+    'danceability',
+    'duration_ms',
+    'energy',
+    'instrumentalness',
+    'key',
+    'liveness',
+    'loudness',
+    'mode',
+    'speechiness',
+    'tempo',
+    'time_signature',
+    'valence'
+]
+msd = pd.read_hdf('../data/full_msd_with_audio_features.h5', key='df')
+msd = msd[['song_id'] + embedding_cols]
+msd.head()
 
-# NOTE: if song_ids are passed in as normal strings, replace instances of song_id with prepare_song_id(song_id)
+def get_list_dissimilarity(song_ids):
+    song_vectors = msd.loc[msd['song_id'].isin(song_ids)][embedding_cols].values
+    return np.mean(pdist(song_vectors, 'cosine'))
 
-def prepare_song_id(song_id):
-  return ('b\'' + song_id + '\'')
-
-
-def get_diversity(song_vector_list):
-  dissim = 0
-  n = len(song_vector_list)
-  print(n)
-  print(n / 2)
-  print(((n / 2) * (n - 1)))
-  for i in range(n):
-    for j in range(n):
-      dissim += np.linalg.norm(song_vector_list[i] - song_vector_list[j])
-
-  return dissim / ((n / 2) * (n - 1))
-
-
-def convert_recs_to_embeddings(song_ids):
-  embeddings_list = []
-
-  for song_id in song_ids:
-    # NOTE: if song_ids are passed in as normal strings, replace all following instances of song_id with prepare_song_id(song_id)
-    if (song_id in songs_df.song_id.values):
-      row = songs_df.loc[songs_df['song_id'] == song_id]
-      embedding = row.iloc[:, 12:]
-      embedding = np.array(embedding.values.tolist()[0])
-      embeddings_list.append(embedding)
-
-  return embeddings_list
-
-
-def get_diversity_score(song_ids):
-  # convert to list of embeddings
-  embedding_list = convert_recs_to_embeddings(song_ids)
-
-  # calculate diversity
-  return get_diversity(embedding_list)
-
-
-song_ids = list(songs_df.head(100).song_id)
-print(len(song_ids))
-print(get_diversity_score(song_ids))
-
-feature_MUSIC_dict = {
-    'danceability': np.array([-0.37, 0.05, -0.35, 0.08, 0.43]),
-    'energy': np.array([-0.64, -0.46, -0.13, 0.66, -0.03]),
-    'instrumentalness': np.array([0.20, -0.47, 0.28, 0.09, -0.01]),
-    'liveness': np.array([-0.69, -0.12, -0.07, 0.43, 0.02]),
-    'loudness': np.array([-0.58, -0.19, -0.44, 0.79, -0.21]),
-    'valence': np.array([-0.04, 0.18, 0.24, -0.34, 0.18]),
-}
+song_ids = list(msd.head(1000).song_id)
+start = time.time()
+print(f'Dissim was {get_list_dissimilarity(song_ids)}')
+print(f'It took {time.time() - start}s')
