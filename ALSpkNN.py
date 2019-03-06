704e3dc3c42d3098a7a3796c29907c6c3ce7835e
Nolan Dey <ndey96@gmail.com>
2019-03-06 04:02:24 -0500

    Speed up ALSpkNN 3x by removing redundant pandas queries

--- ALSpkNN.py
+++ ALSpkNN.py
@@ -39,15 +39,12 @@ class ALSpkNN():
                  song_df,
                  k=100,
                  knn_frac=0.5,
-                 min_overlap=0,
                  cf_weighting_alpha=1):
-
         self.user_df = user_df
         self.song_df = song_df
         self.cf_weighting_alpha = cf_weighting_alpha
         self.knn_frac = knn_frac
         self.k = k
-        self.min_overlap = min_overlap
         self.kdtree = KDTree(user_df['MUSIC'].tolist())
 
         #build the collaborative filtering model with params hardcoded
@@ -60,49 +57,70 @@ class ALSpkNN():
         self.cf_model = AlternatingLeastSquares(**als_params)
 
     def fit(self, train_csr):
+        #don't want to modify original incase it gets put into other models
         weighted_train_csr = weight_cf_matrix(train_csr,
                                               self.cf_weighting_alpha)
         self.cf_model.fit(weighted_train_csr)
 
-    def calculate_overlap(self, list_1, list_2):
-        overlap = len(set(list_1) & set(list_2))
-        total = len(set(list_1)) + len(set(list_2))
+    # def calculate_overlap(self, list_1, list_2):
+    #     overlap = len(set(list_1) & set(list_2))
+    #     total = len(set(list_1)) + len(set(list_2))
 
-        return float(overlap) / total
+    #     return float(overlap) / total
 
-    def get_overlap_list(self, user_id, closest_user_songs):
+    # def get_overlap_list(self, user_id, closest_user_songs):
 
-        overlap_list = []
-        songs = self.user_df.loc[self.user_df['user_id'] == user_id][
-            'song_ids'].values[0]
-        for i in range(len(closest_user_songs)):
-            overlap_list.append(
-                self.calculate_overlap(songs, closest_user_songs[i]))
+    #     overlap_list = []
+    #     songs = self.user_df.loc[self.user_df['user_id'] == user_id][
+    #         'song_ids'].values[0]
+    #     for i in range(len(closest_user_songs)):
+    #         overlap_list.append(
+    #             self.calculate_overlap(songs, closest_user_songs[i]))
 
-        return overlap_list
+    #     return overlap_list
+
+    # # Returns list of song_ids
+    # def get_knn_top_m_songs(self, user_id, m, min_overlap):
+
+    #     user_MUSIC = self.user_df.loc[self.user_df['user_id'] == user_id][
+    #         'MUSIC'].values[0]
+    #     distances, indices = self.kdtree.query(user_MUSIC, self.k, p=1)
+    #     # TODO: maybe sort closest_user_ids by distance if they are not already sorted?
+
+    #     closest_user_ids = self.user_df.iloc[indices]['user_id'].tolist()
+
+    #     # closest_user_songs -> list of lists of song_ids, len(closest_user_songs) == k
+    #     closest_user_songs = self.user_df.loc[self.user_df['user_id'].isin(
+    #         closest_user_ids)]['song_ids'].values
+
+    #     # calculate overlap for all songlists and delete those without enough overlap
+    #     insufficient_overlap_indices = []
+    #     overlap_list = self.get_overlap_list(user_id, closest_user_songs)
+    #     for i in range(len(closest_user_songs)):
+    #         if overlap_list[i] < min_overlap:
+    #             insufficient_overlap_indices.append(i)
+    #     closest_user_songs = np.delete(closest_user_songs,
+    #                                    insufficient_overlap_indices)
+
+    #     # closest_user_songs_flat -> list of song_ids
+    #     closest_user_songs_flat = itertools.chain.from_iterable(
+    #         closest_user_songs)
+
+    #     top_m_songs = [
+    #         i[0] for i in Counter(closest_user_songs_flat).most_common(m)
+    #     ]
+    #     return top_m_songs
 
     # Returns list of song_ids
-    def get_knn_top_m_songs(self, user_id, m, min_overlap):
-
-        user_MUSIC = self.user_df.loc[self.user_df['user_id'] == user_id][
-            'MUSIC'].values[0]
+    def get_knn_top_m_song_ids(self, user_sparse_index, m):
+        user_MUSIC = self.user_df.loc[user_sparse_index]['MUSIC']
         distances, indices = self.kdtree.query(user_MUSIC, self.k, p=1)
-        # TODO: maybe sort closest_user_ids by distance if they are not already sorted?
+        # TODO: maybe sort indices by distance if they are not already sorted?
 
-        closest_user_ids = self.user_df.iloc[indices]['user_id'].tolist()
+        # closest_user_ids = self.user_df.iloc[indices]['user_id'].to_list()
 
         # closest_user_songs -> list of lists of song_ids, len(closest_user_songs) == k
-        closest_user_songs = self.user_df.loc[self.user_df['user_id'].isin(
-            closest_user_ids)]['song_ids'].values
-
-        # calculate overlap for all songlists and delete those without enough overlap
-        insufficient_overlap_indices = []
-        overlap_list = self.get_overlap_list(user_id, closest_user_songs)
-        for i in range(len(closest_user_songs)):
-            if overlap_list[i] < min_overlap:
-                insufficient_overlap_indices.append(i)
-        closest_user_songs = np.delete(closest_user_songs,
-                                       insufficient_overlap_indices)
+        closest_user_songs = self.user_df.loc[indices]['song_ids'].values
 
         # closest_user_songs_flat -> list of song_ids
         closest_user_songs_flat = itertools.chain.from_iterable(
@@ -114,32 +132,24 @@ class ALSpkNN():
         return top_m_songs
 
     # Returns [(song_sparse_index, confidence)]
-    def recommend(self, user_sparse_index, train_plays, N):
+    def recommend(self, user_sparse_index, train_plays_transpose, N):
         # m -> number of songs from KNN recs
         m = int(np.round(self.knn_frac * N))
         # n -> number of songs from CF recs
         n = N - m
 
         n_songs = self.cf_model.recommend(
-            userid=user_sparse_index, user_items=train_plays.transpose(), N=n)
+            userid=user_sparse_index, user_items=train_plays_transpose, N=n)
 
-        # TODO: use a left join to speed this up?
-        user_id = self.user_mapping.loc[self.user_mapping['sparse_index'] ==
-                                        user_sparse_index]['user'].values[0]
-
-        m_songs = self.get_knn_top_m_songs(
-            user_id=user_id, m=m, min_overlap=self.min_overlap)
-
-        # TODO: use a left join to speed this up?
-        m_songs = [
-            self.song_mapping.loc[self.song_mapping.track == song][
-                'sparse_index'].values[0] for song in m_songs
-        ]
+        # user_id = self.user_df.loc[self.user_df['sparse_index'] ==
+        #                            user_sparse_index]['user_id']
+        m_song_ids = self.get_knn_top_m_song_ids(
+            user_sparse_index=user_sparse_index, m=m)
+        m_songs = self.song_df.loc[m_song_ids]['sparse_index'].tolist()
 
         #I don't think score/confidence is used in MAP@k function, so it doesn't matter what value is filled
         hopefully_unimportant_val = 0.69
 
         m_songs = [(song, hopefully_unimportant_val) for song in m_songs]
-        # rec_list = utilities.concat_shuffle(n_songs, m_songs)
-        rec_list = n_songs + m_songs
+        rec_list = utilities.concat_shuffle(n_songs, m_songs)
         return rec_list[:N]
--- evaluation.py
+++ evaluation.py
@@ -1,32 +1,55 @@
 from tqdm import tqdm
 import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix
+from multiprocessing import Pool
+import time
+import pandas as pd
+from scipy.sparse import load_npz
+from ALSpkNN import get_baseline_cf_model, weight_cf_matrix
 
 # https://github.com/benfred/implicit/blob/master/implicit/evaluation.pyx
 
+# http://sdsawtelle.github.io/blog/output/mean-average-precision-MAP-for-recommender-systems.html
+
+
+def get_average_precision(arg_tuple):
+    user_index, listened_song_indices, model, train_user_items = arg_tuple
+    recommended_song_indices = [
+        rec[0]
+        for rec in model.recommend(user_index, train_user_items, N=K)
+    ]
+
+    precision_sum = 0
+    for k in range(1, K):
+        num_correct_recs = len(listened_song_indices.intersection(recommended_song_indices[:k]))
+        precision_sum += num_correct_recs / k
+        precision_sum += precision
+
+    return precision_sum / min(K, len(listened_song_indices))
+
+def multi_mean_average_precision_at_k(model,
+                                   train_user_items,
+                                   test_user_items,
+                                   K=10):
+    user_items_coo = test_user_items.tocoo()
+    user_to_listened_songs_map = {}
+    for user_index, song_index in zip(user_items_coo.row, user_items_coo.col):
+        if user_index not in user_to_listened_songs_map:
+            user_to_listened_songs_map[user_index] = set()
+        user_to_listened_songs_map[user_index].add(song_index)
+
+    arg_tuples = []
+    for user, songs in user_to_listened_songs_map.items():
+        arg_tuples.append((user, songs, model, train_user_items))
+
+    average_precisions = Pool().map(get_average_precision, arg_tuples, chunksize=625)
+
+    return np.mean(average_precisions)
 
 def py_mean_average_precision_at_k(model,
                                    train_user_items,
                                    test_user_items,
                                    K=10):
-    """ Calculates MAP@K for a given trained model
-    Parameters
-    ----------
-    model : RecommenderBase
-        The fitted recommendation model to test
-    train_user_items : csr_matrix
-        csr matrix of user by item that contains elements that were used in training the model
-    test_user_items : csr_matrix
-        csr matrix of user by item that contains withheld elements to test on
-    K : int
-        Number of items to test on
-    Returns
-    -------
-    float
-        the calculated MAP@k
-    """
-
-    
 
     user_items_coo = test_user_items.tocoo()
     user_to_listened_songs_map = {}
@@ -35,61 +58,79 @@ def py_mean_average_precision_at_k(model,
             user_to_listened_songs_map[user_index] = set()
         user_to_listened_songs_map[user_index].add(song_index)
 
-
     average_precision_sum = 0
     for user_index, listened_song_indices in tqdm(user_to_listened_songs_map.items()):
+        start = time.time()
         recommended_song_indices = [
             rec[0]
             for rec in model.recommend(user_index, train_user_items, N=K)
         ]
+        print(f'recs: {time.time() - start}s')
 
-        # http://sdsawtelle.github.io/blog/output/mean-average-precision-MAP-for-recommender-systems.html
+        start = time.time()
         precision_sum = 0
         for k in range(1, K):
-            k_recommended_song_indices = set(recommended_song_indices[:k])
-            num_correct_recs = len(listened_song_indices & k_recommended_song_indices)
-            precision = num_correct_recs / k
-            precision_sum += precision
+            num_correct_recs = len(listened_song_indices.intersection(recommended_song_indices[:k]))
+            precision_sum += num_correct_recs / k
 
-        average_precision = precision_sum / min(K, len(listened_song_indices))
-        average_precision_sum += average_precision
-        # average_precision = 0
-        # relevant = 0
-        #     if likes.find(song_ids[i]) != likes.end():
-        #         relevant += 1
-        #         average_precision += relevant / (i + 1)
-
-        # mean_ap += average_precision / min(K, len(likes))
-        # total += 1
-
-    num_test_users = user_to_listened_songs_map
-    mean_average_precision = average_precision_sum / total_num_users
-    return mean_average_precision
+        average_precision_sum += precision_sum / min(K, len(listened_song_indices))
+        print(f'ap total: {time.time() - start}s')
+    
+    return average_precision_sum / len(user_to_listened_songs_map)
 
 
 if __name__ == '__main__':
-    from ALSpkNN import get_baseline_cf_model, weight_cf_matrix
-    from scipy.sparse import load_npz
-    import time
+    user_df = pd.read_hdf('data/user_df.h5', key='df')[['user_id', 'sparse_index', 'MUSIC', 'song_ids']]
+    # user_df = pd.read_hdf('data/user_df.h5', key='df')
+    user_df.set_index('sparse_index', inplace=True)
 
+    song_df = pd.read_hdf('data/song_df.h5', key='df')[['song_id', 'sparse_index']]
+    # song_df = pd.read_hdf('data/song_df.h5', key='df')
+    song_df.set_index('song_id', inplace=True)
     train_plays = load_npz('data/train_sparse.npz')
     test_plays = load_npz('data/test_sparse.npz')
 
-    # print("Building and fitting the baseline CF model")
-    baseline_cf_model = get_baseline_cf_model()
-    # weighted_train_csr = weight_cf_matrix(train_plays, alpha=1)
-    baseline_cf_model.fit(train_plays)
+    from ALSpkNN import ALSpkNN
 
-    # print("Evaluating the baseline CF model")
+    print("Building model...")
+    model = ALSpkNN(user_df, song_df, k=100, knn_frac=0.5, cf_weighting_alpha=1)
+    print("Fitting model...")
+    model.fit(train_plays)
+    recs = model.recommend(user_sparse_index=12345, train_plays_transpose=train_plays.transpose(), N=5)
+    print(recs)
+
     start = time.time()
-
-    # setting num_threads = 0 yields a 3x speedup on Nolan's MBP
     MAPk = py_mean_average_precision_at_k(
-        model=baseline_cf_model,
-        train_user_items=train_plays.transpose(),
-        test_user_items=test_plays.transpose(),
+        model,
+        train_plays.transpose(),
+        test_plays.transpose(),
         K=5)
+    #     show_progress=False,
+    #     num_threads=0)
 
-    print("MAPK for baseline CF is: " + str(MAPk))
+    print("MAPK for ALSpKNN is: " + str(MAPk))
     print(f'Calculation took {time.time() - start}s')
+
+    ##################################################################
+
+    # train_plays = load_npz('data/train_sparse.npz')
+    # test_plays = load_npz('data/test_sparse.npz')
+
+    # # print("Building and fitting the baseline CF model")
+    # baseline_cf_model = get_baseline_cf_model()
+    # # weighted_train_csr = weight_cf_matrix(train_plays, alpha=1)
+    # baseline_cf_model.fit(train_plays)
+
+    # # print("Evaluating the baseline CF model")
+    # start = time.time()
+
+    # # setting num_threads = 0 yields a 3x speedup on Nolan's MBP
+    # MAPk = py_mean_average_precision_at_k(
+    #     model=baseline_cf_model,
+    #     train_user_items=train_plays.transpose(),
+    #     test_user_items=test_plays.transpose(),
+    #     K=5)
+
+    # print("MAPK for baseline CF is: " + str(MAPk))
+    # print(f'Calculation took {time.time() - start}s')
 
