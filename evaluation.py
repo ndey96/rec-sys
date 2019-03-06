e2d0fa2e5e2b554a1167e678007949dd4cba79b2
Nolan Dey <ndey96@gmail.com>
2019-03-06 04:56:44 -0500

    Only parallelize the recommending to minimize worker downtime

--- evaluation.py
+++ evaluation.py
@@ -11,39 +11,57 @@ from functools import partial
 
 # http://sdsawtelle.github.io/blog/output/mean-average-precision-MAP-for-recommender-systems.html
 
-
-def get_average_precision(arg_tuple, model, train_user_items):
-    user_index, listened_song_indices = arg_tuple
+def get_user_recs(user_index, model, train_user_items, K):
     recommended_song_indices = [
         rec[0]
         for rec in model.recommend(user_index, train_user_items, N=K)
     ]
+    return {user_index: recommended_song_indices}
 
-    precision_sum = 0
-    for k in range(1, K):
-        num_correct_recs = len(listened_song_indices.intersection(recommended_song_indices[:k]))
-        precision_sum += num_correct_recs / k
-        precision_sum += precision
-
-    return precision_sum / min(K, len(listened_song_indices))
 
 def multi_mean_average_precision_at_k(model,
                                    train_user_items,
                                    test_user_items,
                                    K=10):
     user_items_coo = test_user_items.tocoo()
+
+    # user_to_listened_songs_map -> {user_index: listened_song_indices}
     user_to_listened_songs_map = {}
     for user_index, song_index in zip(user_items_coo.row, user_items_coo.col):
         if user_index not in user_to_listened_songs_map:
             user_to_listened_songs_map[user_index] = set()
         user_to_listened_songs_map[user_index].add(song_index)
 
+    start = time.time()
     print('Starting pool.map')
-    average_precisions = Pool().map(
-        partial(get_average_precision, model=model, train_user_items=train_user_items),
-        user_to_listened_songs_map.items(),
+    # user_recs -> [{user_index: recommended_song_indices}]
+    user_recs = Pool().map(
+        func=partial(get_user_recs, model=model, train_user_items=train_user_items, K=K),
+        iterable=user_to_listened_songs_map.keys(),
         chunksize=625
     )
+    print(f'recs: {time.time() - start}s')
+
+    start = time.time()
+    # user_to_recs_map -> {user_index: recommended_song_indices}
+    user_to_recs_map = {}
+    for user_rec_dict in user_recs:
+        user_to_recs_map.update(user_rec_dict)
+
+    average_precision_sum = 0
+    for user_index in user_to_listened_songs_map.keys():
+        listened_song_indices = user_to_listened_songs_map[user_index]
+        recommended_song_indices = user_to_recs_map[user_index]
+
+        precision_sum = 0
+        for k in range(1, K):
+            num_correct_recs = len(listened_song_indices.intersection(recommended_song_indices[:k]))
+            precision_sum += num_correct_recs / k
+            precision_sum += precision
+
+        average_precision_sum += precision_sum / min(K, len(listened_song_indices))
+
+    print(f'MAP: {time.time() - start}s')
 
     return np.mean(average_precisions)
 
