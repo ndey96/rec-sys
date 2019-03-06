ad3907215bdff2f75aa70bb1452fa22aa7487281
Nolan Dey <ndey96@gmail.com>
2019-03-06 01:33:08 -0500

    Speed up MAPK 10x by creating user songs map and by using coo matrix

--- evaluation.py
+++ evaluation.py
@@ -1,14 +1,6 @@
-from tqdm import trange
+from tqdm import tqdm
 import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix
-
-# import cython
-# from cython.operator import dereference
-# from cython.parallel import parallel, prange
-# from libc.stdlib cimport malloc, free
-# from libc.string cimport memset
-# from libc.math cimport fmin
-# from libcpp.unordered_set cimport unordered_set
 
 # https://github.com/benfred/implicit/blob/master/implicit/evaluation.pyx
 
@@ -16,9 +8,7 @@ from scipy.sparse import coo_matrix, csr_matrix
 def py_mean_average_precision_at_k(model,
                                    train_user_items,
                                    test_user_items,
-                                   K=10,
-                                   show_progress=True,
-                                   num_threads=1):
+                                   K=10):
     """ Calculates MAP@K for a given trained model
     Parameters
     ----------
@@ -30,51 +20,35 @@ def py_mean_average_precision_at_k(model,
         csr matrix of user by item that contains withheld elements to test on
     K : int
         Number of items to test on
-    show_progress : bool, optional
-        Whether to show a progress bar
-    num_threads : int, optional
-        The number of threads to use for testing. Specifying 0 means to default
-        to the number of cores on the machine. Note: aside from the ALS and BPR
-        models, setting this to more than 1 will likely hurt performance rather than
-        help.
     Returns
     -------
     float
         the calculated MAP@k
     """
 
-    total = 0
-    total_num_users = test_user_items.shape[0]
-    test_indptr = test_user_items.indptr
+    
 
-    # Supposed to represent song_indices I think?
-    # Should be user indices in practice though
-    test_indices = test_user_items.indices
+    user_items_coo = test_user_items.tocoo()
+    user_to_listened_songs_map = {}
+    for user_index, song_index in zip(user_items_coo.row, user_items_coo.col):
+        if user_index not in user_to_listened_songs_map:
+            user_to_listened_songs_map[user_index] = set()
+        user_to_listened_songs_map[user_index].add(song_index)
+
 
     average_precision_sum = 0
-    for u in trange(total_num_users):
-        # if we don't have any test items, skip this user
-        if test_indptr[u] == test_indptr[u + 1]:
-            continue
-
-        # recommended_song_indices -> the set of songs that were recommended
-        recommended_song_indices = {
+    for user_index, listened_song_indices in tqdm(user_to_listened_songs_map.items()):
+        recommended_song_indices = [
             rec[0]
-            for rec in model.recommend(u, train_user_items, N=K)
-        }
-
-        # listened_song_indices -> the set of songs that user has listened to
-        listened_song_indices = {
-            test_indices[i]
-            for i in range(test_indptr[u], test_indptr[u + 1])
-        }
+            for rec in model.recommend(user_index, train_user_items, N=K)
+        ]
 
         # http://sdsawtelle.github.io/blog/output/mean-average-precision-MAP-for-recommender-systems.html
         precision_sum = 0
         for k in range(1, K):
-            num_recs_in_listening_history = len(
-                listened_song_indices & recommended_song_indices)
-            precision = num_recs_in_listening_history / k
+            k_recommended_song_indices = set(recommended_song_indices[:k])
+            num_correct_recs = len(listened_song_indices & k_recommended_song_indices)
+            precision = num_correct_recs / k
             precision_sum += precision
 
         average_precision = precision_sum / min(K, len(listened_song_indices))
@@ -88,6 +62,7 @@ def py_mean_average_precision_at_k(model,
         # mean_ap += average_precision / min(K, len(likes))
         # total += 1
 
+    num_test_users = user_to_listened_songs_map
     mean_average_precision = average_precision_sum / total_num_users
     return mean_average_precision
 
@@ -113,9 +88,7 @@ if __name__ == '__main__':
         model=baseline_cf_model,
         train_user_items=train_plays.transpose(),
         test_user_items=test_plays.transpose(),
-        K=5,
-        show_progress=True,
-        num_threads=0)
+        K=5)
 
     print("MAPK for baseline CF is: " + str(MAPk))
     print(f'Calculation took {time.time() - start}s')
