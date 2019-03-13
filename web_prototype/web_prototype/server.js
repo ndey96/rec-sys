e26e765bd9fbee5b3aa749efcd7b9d350da3b764
Rishi <goelrishi12@gmail.com>
2019-03-12 21:04:12 -0400

    we getting recd

--- web_prototype/web_prototype/server.js
+++ web_prototype/web_prototype/server.js
@@ -2,28 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-/*app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});*/
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/proto', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
