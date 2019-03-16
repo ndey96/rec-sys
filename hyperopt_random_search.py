1969135a7e0f84c05c1e9b1a5799323599fa95d6
Nolan Dey <ndey96@gmail.com>
2019-03-16 12:30:03 -0400

    Add try catch to random search

--- hyperopt_random_search.py
+++ hyperopt_random_search.py
@@ -34,37 +34,39 @@ if __name__ == '__main__':
         file.write('k,knn_frac,max_overlap,mode,map_k,cosine,metadata\n')
     
     while True:
-        print("Building model...")
-        model = ALSpkNN(
-            user_df,
-            song_df,
-            k=int(log_uniform(10, 10000)),
-            knn_frac=random.uniform(0, 1),
-            cf_weighting_alpha=1,
-            max_overlap=random.uniform(0, 1),
-            mode=random.choice(['popular', 'weighted_random', 'random']),
-            min_songs=int(random.uniform(1, 15))
-        )
-        print("Fitting model...")
-        model.fit(train_plays)
-        
-        print(f'Evaluating ALSpkNN with k={model.k}, knn_frac={model.knn_frac}, max_overlap={model.max_overlap}, min_songs={model.min_songs}')
+        try:
+            print("Building model...")
+            model = ALSpkNN(
+                user_df,
+                song_df,
+                k=int(log_uniform(10, 10000)),
+                knn_frac=random.uniform(0, 1),
+                cf_weighting_alpha=1,
+                max_overlap=random.uniform(0, 1),
+                mode=random.choice(['popular', 'weighted_random', 'random']),
+                min_songs=int(random.uniform(1, 15))
+            )
+            print("Fitting model...")
+            model.fit(train_plays)
+            
+            print(f'Evaluating ALSpkNN with k={model.k}, knn_frac={model.knn_frac}, max_overlap={model.max_overlap}, min_songs={model.min_songs}')
 
-        metrics = evaluation_hyperopt.get_metrics(
-            metrics=['MAP@K', 'mean_cosine_list_dissimilarity', 'metadata_diversity'],
-            N=N,
-            model=model,
-            train_user_items=train_plays.transpose(),
-            test_user_items=test_plays.transpose(),
-            song_df=song_df,
-            limit=USER_LIMIT)
-        
-        mapk = metrics["MAP@K"]
-        cosdis = metrics["mean_cosine_list_dissimilarity"]
-        metadata = metrics["metadata_diversity"]
-        
-        with open(file_path, 'a') as file:
-            file.write(f'{model.k},{model.knn_frac},{model.max_overlap},{model.mode},{mapk},{cosdis},{metadata}\n')
-
+            metrics = evaluation_hyperopt.get_metrics(
+                metrics=['MAP@K', 'mean_cosine_list_dissimilarity', 'metadata_diversity'],
+                N=N,
+                model=model,
+                train_user_items=train_plays.transpose(),
+                test_user_items=test_plays.transpose(),
+                song_df=song_df,
+                limit=USER_LIMIT)
+            
+            mapk = metrics["MAP@K"]
+            cosdis = metrics["mean_cosine_list_dissimilarity"]
+            metadata = metrics["metadata_diversity"]
+            
+            with open(file_path, 'a') as file:
+                file.write(f'{model.k},{model.knn_frac},{model.max_overlap},{model.mode},{mapk},{cosdis},{metadata}\n')
+        except Exception as e:
+            print(f'Ran into the following exception: {e}')
         
 
