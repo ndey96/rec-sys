dfa8a2078bccc872689526b6bcb3e3eb39ef07a9
MatthewMcLeod <mkmmcleod@gmail.com>
2019-04-12 00:01:49 -0400

    Adding weighted recommender to the models init for importing

--- models/WeightedRecommender.py
+++ models/WeightedRecommender.py
@@ -25,26 +25,11 @@ class WeightedRecommender:
         else:
             songs_listened_to = []
             
-        
         filtered_songs = filter(lambda x: filter_songs_func(x,songs_listened_to),self.song_counts)
-        
         filtered_songs = list(filtered_songs)
         
         songs_possible = [song_tuple[0] for song_tuple in filtered_songs]
         songs_prob = [song_tuple[1] for song_tuple in filtered_songs]
-
-        
-#        matching_indices = []
-#        for ind, song_id in enumerate(self.songs_ranked):
-#            if song_id in songs_listened_to:
-#                matching_indices.append(ind)
-#
-#        # make a copy of array so you don't delete songs everytime recommend is ran
-#        possible_songs = self.songs_ranked[:]
-#        play_counts = self.songs_plays_ranked[:]
-#        for ind in matching_indices:
-#            possible_songs.pop(ind)
-#            play_counts.pop(ind)
 
         songs_prob = np.array(songs_prob)
         songs_prob = songs_prob / songs_prob.sum()
@@ -57,10 +42,7 @@ class WeightedRecommender:
         #csr matrix of item by users in same format as fit for CF model
         item_user = train_csr.tocoo()
         all_songs = item_user.row
-        #remove duplicate song_sparse_id_entries
-        #        self.unique_songs = set(all_songs)
-        self.user_to_listened_songs_map = build_songs_listened_to_map(item_user)
 
+        self.user_to_listened_songs_map = build_songs_listened_to_map(item_user)
         self.song_counts = Counter(all_songs).most_common()
-#        self.songs_ranked = [song_tuple[0] for song_tuple in self.song_counts]
-#        self.songs_plays_ranked = [song_tuple[1] for song_tuple in self.song_counts]
+
--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .PopularRecommender import PopularRecommender
 from .RandomRecommender import RandomRecommender
 from .WeightedRecommender import WeightedRecommender
 from .ALS import ALSRecommender
+from .RandomUserRecommender import RandomUserRecommender
