0e2a6c555a3034e8eca2237ff2172dcecd9e2360
MatthewMcLeod <mkmmcleod@gmail.com>
2019-04-11 23:58:16 -0400

    Weighted Recommender fix

--- models/WeightedRecommender.py
+++ models/WeightedRecommender.py
@@ -10,6 +10,12 @@ from collections import Counter
 from .utils import build_songs_listened_to_map
 
 
+def filter_songs_func(song_count_tuple,song_blacklist):
+    if song_count_tuple[0] in song_blacklist:
+        return False
+    else:
+        return True
+
 class WeightedRecommender:
 
     def recommend(self, user_sparse_index, train_plays_transpose=None, N=20):
@@ -18,24 +24,33 @@ class WeightedRecommender:
                 user_sparse_index]
         else:
             songs_listened_to = []
+            
+        
+        filtered_songs = filter(lambda x: filter_songs_func(x,songs_listened_to),self.song_counts)
+        
+        filtered_songs = list(filtered_songs)
+        
+        songs_possible = [song_tuple[0] for song_tuple in filtered_songs]
+        songs_prob = [song_tuple[1] for song_tuple in filtered_songs]
 
-        matching_indices = []
-        for ind, song_id in enumerate(self.songs_ranked):
-            if song_id in songs_listened_to:
-                matching_indices.append(ind)
+        
+#        matching_indices = []
+#        for ind, song_id in enumerate(self.songs_ranked):
+#            if song_id in songs_listened_to:
+#                matching_indices.append(ind)
+#
+#        # make a copy of array so you don't delete songs everytime recommend is ran
+#        possible_songs = self.songs_ranked[:]
+#        play_counts = self.songs_plays_ranked[:]
+#        for ind in matching_indices:
+#            possible_songs.pop(ind)
+#            play_counts.pop(ind)
 
-        # make a copy of array so you don't delete songs everytime recommend is ran
-        possible_songs = self.songs_ranked[:]
-        play_counts = self.songs_plays_ranked[:]
-        for ind in matching_indices:
-            possible_songs.pop(ind)
-            play_counts.pop(ind)
-
-        song_probs = np.array(play_counts)
-        song_probs = song_probs / song_probs.sum()
+        songs_prob = np.array(songs_prob)
+        songs_prob = songs_prob / songs_prob.sum()
 
         recs = np.random.choice(
-            a=possible_songs, p=song_probs, size=N, replace=False)
+            a=songs_possible, p=songs_prob, size=N, replace=False)
         return recs
 
     def fit(self, train_csr):
@@ -46,6 +61,6 @@ class WeightedRecommender:
         #        self.unique_songs = set(all_songs)
         self.user_to_listened_songs_map = build_songs_listened_to_map(item_user)
 
-        song_counts = Counter(all_songs).most_common()
-        self.songs_ranked = [song_tuple[0] for song_tuple in song_counts]
-        self.songs_plays_ranked = [song_tuple[1] for song_tuple in song_counts]
+        self.song_counts = Counter(all_songs).most_common()
+#        self.songs_ranked = [song_tuple[0] for song_tuple in self.song_counts]
+#        self.songs_plays_ranked = [song_tuple[1] for song_tuple in self.song_counts]
